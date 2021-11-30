3403635143f51a551af734acca7a983ac8794aa5
Mario <marioballestero.rdg@gmail.com>
2021-11-30 18:35:48 +0100

    feat(Tag): add component styles

--- src/components/Tag/styles.ts
+++ src/components/Tag/styles.ts
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 100px;
+  height: fit-content;
+  padding: 0.125rem 0.625rem;
+  border-radius: 0.5rem;
+  background-color: #ebdcf9;
+  width: fit-content;
   display: flex;
-  flex-direction: column;
+  align-items: center;
 `;
 
-export const Text = styled.p`
-  color: red;
+export const Text = styled.span`
+  font-weight: 500;
+  font-size: 0.625rem;
+  line-height: 0.875rem;
+  letter-spacing: -0.035em;
+  color: #9b51e0;
 `;
