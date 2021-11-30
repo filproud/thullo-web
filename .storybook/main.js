81eb24c34fb0653c768f1d09f156b04d0be77111
Mario <marioballestero.rdg@gmail.com>
2021-11-30 18:35:08 +0100

    feat(Tag): add component stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/stories.mdx', '../src/**/stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-knobs',
---
+++ src/components/Tag/Tag.stories.tsx
@@ -0,0 +1,10 @@
+import type { Meta } from '@storybook/react';
+
+import { Tag } from '.';
+
+export default {
+  component: Tag,
+  title: 'Tag',
+} as Meta;
+
+export const Default = () => <Tag />;
