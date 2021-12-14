ec7bb513e8460eb9a73cfc53e5044388e2e37de5
sergio-av <sergioavilachacon@gmail.com>
2021-12-14 20:11:50 +0100

    feat(Logo): logo component type declared

--- src/components/Logo/types.ts
+++ src/components/Logo/types.ts
@@ -1,8 +1,4 @@
-import type { ReactNode } from 'react';
-
 export type Props = {
-  username: string;
-  children?: ReactNode;
-  className?: string;
-  open?: boolean;
+  title: string;
+  img: string;
 };
