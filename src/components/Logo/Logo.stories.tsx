00f73495686ee6d7cdb72c1198e0fd0ced37b975
sergio-av <sergioavilachacon@gmail.com>
2021-12-16 01:04:46 +0100

    feat(Logo): component Logo done

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -1,12 +1,11 @@
 import type { Meta } from '@storybook/react';
 
 import { Logo } from '.';
+import iconLogo from '../../assets/icons/Logo-small.svg';
 
 export default {
   component: Logo,
   title: 'Logo',
 } as Meta;
 
-export const Default = () => <Logo title="Thullo" />;
-
-export const Disabled = () => <Logo title="Thullo" />;
+export const Default = () => <Logo title="Thullo" url={iconLogo} />;
